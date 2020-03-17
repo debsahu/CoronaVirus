ceb07fd8023ce903fbc159e39a7b5ea936bd61cb
Debashish Sahu <debashish.sahu@gmail.com>
2020-03-17 10:19:43 -0400

    3/17/20 update
    https://www.michigan.gov/coronavirus/0,9753,7-406-98163-521887--,00.html

--- coronavirus.py
+++ coronavirus.py
@@ -1,15 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
+import datetime
 
+x = datetime.datetime.now()
+date = x.strftime("%D")
 page = requests.get('https://www.michigan.gov/coronavirus')
 
 soup = BeautifulSoup(page.text, 'html.parser')
-th = soup.find_all('span', {'class': 'shortdesc'})[1].find_all('td')
+pos = soup.find_all('span', {'class': 'shortdesc'})[1].find_all('strong')[4].text.strip()
 
-date = th[1].text.strip()
-all_tested = th[3].text.strip()
-neg = th[5].text.strip()
-pos = th[7].text.strip()
-pending = th[9].text.strip()
+print(pos + " confirmed COVID-19 cases in Michigan as of " + date)
 
-print(pos + " confirmed COVID-19 cases in Michigan on " + date)
+report = {}
+report["value1"] = pos
+report["value2"] = date
+requests.post("https://maker.ifttt.com/trigger/covid19/with/key/XXXXXXXXXXXXXXXXXXXX", data=report)
