e70c9e5ba15961e60ee6efb93801417a21589367
Debashish Sahu <debashish.sahu@gmail.com>
2020-04-16 16:18:25 -0400

    Update coronavirus.py

--- coronavirus.py
+++ coronavirus.py
@@ -1,30 +1,34 @@
+#%% Import Libraries
 import requests
 from bs4 import BeautifulSoup
 import datetime
 import ifttt_config
 
+#%% Global Variables
 x = datetime.datetime.now()
 date = x.strftime("%D %T")
 
 base_url = 'https://www.michigan.gov'
 corona_main_page = requests.get(base_url + '/coronavirus')
 
-## Parse link to statistics page
+#%% Parse link to statistics page
 soup_link = BeautifulSoup(corona_main_page.text, 'html.parser')
-# link = soup_link.find('div', id="block-3_114405").find('span', class_='readLink').find('a', href=True)['href']
 link = soup_link.find('a', string='See Cumulative Data', href=True)['href']
 # print(link)
 
-## Follow the link and parse total michigan statistics
+#%% Follow the link and parse total michigan statistics
 page = requests.get(base_url + link)
 soup = BeautifulSoup(page.text, 'html.parser')
-pos = soup.find('table').find_all("tr")[-1].find_all("td")[-2].text.strip()
-death = soup.find('table').find_all("tr")[-1].find_all("td")[-1].text.strip()
+pos = soup.find('table').find_all("tr")[-1].find_all("td")[-3].text.strip()
+death = soup.find('table').find_all("tr")[-1].find_all("td")[-2].text.strip()
 # print(pos)
 print("COVID-19: " + pos + " cases & " + death + " deaths in Michigan as of " + date)
 
+#%% Generate IFTTT output
 report = {}
 report["value1"] = pos
 report["value2"] = death
 report["value3"] = date
+
+#%% Send IFTTT request
 requests.post("https://maker.ifttt.com/trigger/covid19/with/key/" + ifttt_config.api_key, data=report)
